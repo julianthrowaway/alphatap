cc76db493d7aa5dd337caaff10face68c1197e2b
Julian Miller <julianmiller520@gmail.com>
2012-01-24 08:33:25 -0600

    project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alphatap::Application.config.secret_token = '4f76f8956ed6b5fbe3850d6ee9b7df33e2d839358bb87c54b17a460ba5ada4d04135834f298d6858b309f00d32084c44c0122655a10a0b2f4f794a5e9101b8d8'
